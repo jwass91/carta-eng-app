dd132e79bf6577b99d5cfc362435245b6b976734
jwass91 <jwass91@gmail.com>
2019-11-28 15:21:37 -0500

    adding routing to views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import "./App.css";
+import { Container } from "semantic-ui-react";
+import { EpisodeView, HomeView, SeasonView } from "./views";
+import { ScrollToTop } from "./components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <ScrollToTop />
+            <Container style={{ margin: "3em" }}>
+                <div>
+                    <Switch>
+                        <Route
+                            path="/season/:number"
+                            render={props => {
+                                return <SeasonView number={props.match.params.number} />;
+                            }}
+                        />
+                        <Route
+                            path="/episode/:season/:number"
+                            render={props => {
+                                return (
+                                    <EpisodeView
+                                        season={props.match.params.season}
+                                        number={props.match.params.number}
+                                    />
+                                );
+                            }}
+                        />
+                        <Route
+                            path="/"
+                            render={props => {
+                                return <HomeView />;
+                            }}
+                        />
+                    </Switch>
+                </div>
+            </Container>
+        </Router>
+    );
 }
 
 export default App;
