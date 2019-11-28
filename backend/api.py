19dffccc732a39127c364e72dcd3843cc9932edb
jwass91 <jwass91@gmail.com>
2019-11-28 15:16:49 -0500

    fixed cors issue & paths

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, abort
-import json, random
+import json
+import random
 
 app = Flask(__name__)
 
@@ -11,54 +12,66 @@ detailed_episodes = []
 with open('../data/himym-seasons.json', 'r') as f:
     seasons_raw = json.load(f)
     for season in seasons_raw:
-    	seasons.append({
-    		'number': season['number'],
-    		'numEpisodes': season['episodeOrder'],
-			'premiereDate': season['premiereDate'],
-			'endDate': season['endDate'],
-    	})
-    	episodes.append([]) # initialize empty array for each season
-    	detailed_episodes.append([]) # initialize empty array for each season
+        seasons.append({
+            'number': season['number'],
+            'numEpisodes': season['episodeOrder'],
+            'premiereDate': season['premiereDate'],
+            'endDate': season['endDate'],
+        })
+        episodes.append([])  # initialize empty array for each season
+        detailed_episodes.append([])  # initialize empty array for each season
 
 # import and initialize episode data
 with open('../data/himym-episodes.json', 'r') as f:
     episodes_raw = json.load(f)
     for episode in episodes_raw['_embedded']['episodes']:
-    	episodes[episode['season'] - 1].append({
-    		'name': episode['name'],
-    		'season': episode['season'],
-    		'number': episode['number'],
-    		'image': episode['image']['original'] if episode['image'] else None
-    	})
-    	detailed_episodes[episode['season'] - 1].append({
-    		'name': episode['name'],
-    		'season': episode['season'],
-    		'number': episode['number'],
-    		'airdate': episode['airdate'],
-    		'summary': episode['summary'],
-    		'image': episode['image']['original'] if episode['image'] else None
-    	})
+        episodes[episode['season'] - 1].append({
+            'name': episode['name'],
+            'season': episode['season'],
+            'number': episode['number'],
+            'image': episode['image']['original'] if episode['image'] else None
+        })
+        detailed_episodes[episode['season'] - 1].append({
+            'name': episode['name'],
+            'season': episode['season'],
+            'number': episode['number'],
+            'airdate': episode['airdate'],
+            'summary': episode['summary'],
+            'image': episode['image']['original'] if episode['image'] else None
+        })
+
 
 @app.route('/seasons/', methods=['GET'])
 def get_seasons():
-    return jsonify(seasons)
+    response = jsonify(seasons)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+
 
 @app.route('/episodes/<int:season_num>/', methods=['GET'])
 def get_seasons_episodes(season_num):
-	if len(episodes) < season_num:
-		abort(404)
-	return jsonify(episodes[season_num - 1])
+    if len(episodes) < season_num:
+        abort(404)
+    response = jsonify(episodes[season_num - 1])
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
-@app.route('/episodes/<int:season_num>/<int:epsiode_num>', methods=['GET'])
+
+@app.route('/episodes/<int:season_num>/<int:epsiode_num>/', methods=['GET'])
 def get_episode(season_num, epsiode_num):
-	if len(detailed_episodes) < season_num:
-		abort(404)
-	if len(detailed_episodes[season_num - 1]) < epsiode_num:
-		abort(404)
-	return jsonify(detailed_episodes[season_num - 1][epsiode_num - 1])
+    if len(detailed_episodes) < season_num:
+        abort(404)
+    if len(detailed_episodes[season_num - 1]) < epsiode_num:
+        abort(404)
+    response = jsonify(detailed_episodes[season_num - 1][epsiode_num - 1])
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
-@app.route('/random_episode', methods=['GET'])
+
+@app.route('/random_episode/', methods=['GET'])
 def get_random_episode():
-	# this method favors episodes in seasosn with fewer episdoes but
-	# was chosen intentionally for simplicity
-	return jsonify(random.choice(random.choice(episodes)))
+    # this method favors episodes in seasosn with fewer episdoes but
+    # was chosen intentionally for simplicity
+    response = jsonify(random.choice(random.choice(episodes)))
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
